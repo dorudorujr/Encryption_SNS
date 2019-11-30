2bbea9ef6e5a44e51f2feb613e59f4d97c966183
seiyasugioka <sugiokaseiya@team-lab.com>
2019-11-30 19:04:48 +0900

    viewファイル作成

--- encryption_sns/lib/Widget/release_raised_button.dart
+++ encryption_sns/lib/Widget/release_raised_button.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:encryption_sns/process_view.dart';
+import 'package:encryption_sns/View/process_view.dart';
 import 'package:encryption_sns/application_bloc_provider.dart';
 
 class ReleaseRaisedButton extends StatelessWidget {
